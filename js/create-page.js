d7b667470640c93d29ff036e6e2547cbb5059c3b
QuangBach111 <buiquangbach25101996@gmail.com>
2023-05-16 19:29:54 +0700

    update storage

--- js/create-page.js
+++ js/create-page.js
@@ -1,98 +1,125 @@
-window.onload = function () {
-    // Add answer btn
-    $(document).on('click', '.btn-add-answer', function () {
-        var answer = $(this).closest('.answer').clone();
-        // Clear value in input
-        answer.find("input[type='text']").val('');
-        $(this).closest('.answer-list').append(answer);
-    });
 
-    //  Add question btn
-    $('#btn-add-question').click(function () {
-        var question = $('.question:last').clone();
-        question.find("input[type='text']").val('');
-        // set at default answer (only one answer)
-        question.find('.answer:not(:first-child)').remove();
-        $('.question-list').append(question);
-    });
+    $(document).ready(function () {
+        //load navbar
+        $(function() {
+            $("#navbar").load("login.html");
+        });
 
-    // Submit event
-    $('form').submit(function (event) {
-        event.preventDefault();
+        // Add answer btn
+        $(document).on('click', '.btn-add-answer', function () {
+            var answer = $(this).closest('.answer').clone();
+            // Clear value in input
+            answer.find("input[type='text']").val('');
+            $(this).closest('.answer-list').append(answer);
+        });
 
-        // Create poll obj
-        var pollObj = {
-            name: String,
-            question: [],
-            status: 'active'
-        };
+        //  Add question btn
+        $('#btn-add-question').click(function () {
+            var question = $('.question:last').clone();
+            question.find("input[type='text']").val('');
+            // set at default answer (only one answer)
+            question.find('.answer:not(:first-child)').remove();
+            $('.question-list').append(question);
+        });
 
-        // poll name
-        pollObj.name = $('.name-poll-input').val();
+        // Submit event
+        $('form').submit(function (event) {
+            event.preventDefault();
 
-        // Question loop
-        var $questionList = $('.question');
-        $questionList.each(function () {
-            // Create questionObj
-            var questionObj = {
-                questionContent: String,
-                answer: []
+            // Create poll obj
+            var pollObj = {
+                name: String,
+                questionList: [],
+                status: 'active'
             };
 
-            // Get question input
-            questionObj.questionContent = $(this).find('.question-input').val();
-            console.log(`Question: ${questionObj.questionContent}`)
+            // poll name
+            pollObj.name = $('.name-poll-input').val();
 
-            // LOOP: answer
-            var $answerList =  $(this).find('.answer-field >.answer-list > .answer');
-            $answerList.each(function () {
-
-                // Create answer obj
-                var answerObj = {
-                    answerContent: String,
-                    result: Boolean
+            // Question loop
+            var $questionList = $('.question');
+            $questionList.each(function () {
+                // Create questionObj
+                var questionObj = {
+                    questionContent: String,
+                    answerList: []
                 };
 
-                // Get answer content
-                answerObj.answerContent = $(this).find('.answer-input').val();
-                console.log(`\tanswer:${answerObj.answerContent} `)
+                // Get question input
+                questionObj.questionContent = $(this).find('.question-input').val();
+                console.log(`Question: ${questionObj.questionContent}`)
 
-                // Add answerObj to questionObj
-                questionObj.answer.push(answerObj);
+                // LOOP: answer
+                var $answerList =  $(this).find('.answer-field >.answer-list > .answer');
+                $answerList.each(function () {
+
+                    // Create answer obj
+                    var answerObj = {
+                        answerContent: String,
+                        status: Boolean
+                    };
+
+                    // Get answer content
+                    answerObj.answerContent = $(this).find('.answer-input').val();
+                    console.log(`\tanswer:${answerObj.answerContent} `)
+
+                    // Add answerObj to questionObj
+                    questionObj.answerList.push(answerObj);
+                });
+
+                // Add questionObj to pollObj
+                pollObj.questionList.push(questionObj);
             });
 
+            // get listItem from localStorage
+            var itemList = localStorage.getItem('itemList');
+            var item = null;
+
+            // Convert listItem to array
+            itemList = JSON.parse(itemList);
+
+            // Test: create newItem
+            var newItem = {
+                user: {
+                    name: John,
+                    password: 1234,
+                    isLogin: true
+                },
+                pollList: []
+            };
+
+            newItem.pollList.push(pollObj);
+
+            itemList.push(newItem);
+
+            // listItems is null
+            if(!itemList){
+                alert('Please login');
+            }
+            else{
+                // Find the user login
+                item = itemList.filter(index => {
+                    return index.user.isLogin === true;
+                });
+
+                // Push pollObj to item
+                item.pollList.push(pollObj);
+
+                itemList = JSON.stringify(itemList);
+                localStorage.setItem('itemList', itemList);
+
+                $.ajax({
+                   type: 'GET',
+                    url: 'http://127.0.0.1:5500/',
+                    success: function(resp) {
+                        window.location.href = 'home-page.html';
+                    },
+                    error: function(error) {
+                        console.log(error);
+                    }
+                });
+            }
 
 
-
-            // Add questionObj to pollObj
-            pollObj.question.push(questionObj);
-        })
-
-        // test
-        // console.log(`Poll name: ${pollObj.name}`);
-
-        // pollObj.question.forEach(question => {
-        //    console.log(`\tquestion: ${question.questionContent}`);
-        //
-        //    question.answer.forEach(answer => {
-        //        console.log(`\t\tanswer: ${answer.answerContent}`);
-        //    });
-        // });
-
-        // // get listItem from localStorage
-        // var listItems = localStorage.getItem('listItems');
-        //
-        // // Convert listItem to array
-        // listItems = JSON.parse(listItems);
-        //
-        // // Base on User to storage
-        // if(!listItems){
-        //     // Create
-        // }
-        // else{
-        //
-        // }
-    });
-
-
-}
+        });
+    })
